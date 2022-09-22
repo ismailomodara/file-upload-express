4fad4d2ad1a594f5e8bd97369759340c1cd24c0a
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-09-22 07:31:50 +0100

    Implemented FileUpload with Cloudinary

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const connectDB = require('./db/connect');
 
 // file upload
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
 
 // product router
 const products = require("./routes/productRoutes");
@@ -18,7 +24,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.static("./public"))
-app.use(fileUpload())
+app.use(fileUpload({ useTempFiles: true }))
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2
 const { StatusCodes } = require("http-status-codes");
 
-const upload = async (req, res) => {
+const uploadLocal= async (req, res) => {
     const productImage = req.files.image;
 
     const imagePath = path.join(__dirname, `../public/uploads/${productImage.name}`);
@@ -9,6 +11,21 @@ const upload = async (req, res) => {
     await productImage.mv(imagePath)
     return res.status(StatusCodes.OK).json({
         image: { src: `/uploads/${productImage.name}` }
+    })
+}
+
+const upload = async (req, res) => {
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+        use_filename: true,
+        folder: 'file-upload'
+    })
+
+    fs.unlinkSync(req.files.image.tempFilePath);
+
+    res.status(StatusCodes.OK).json({
+        image: {
+            src: result.url
+        }
     })
 }
 
